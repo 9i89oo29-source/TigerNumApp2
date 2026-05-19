3705d9d9406d1820bddde05a34cb2d14747ece1b
9i89oo29-source <9i89oo29@gmail.com>
2026-05-19 20:42:36 +0300

    Create BuyRequest.kt

---
+++ app/src/main/java/com/tigernum/app/data/remote/dto/BuyRequest.kt
@@ -0,0 +1,9 @@
+package com.tigernum.app.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BuyRequest(
+    val serviceId: String,
+    val countryCode: String
+)
